ce07068a6f0f4b075072142ed5be364d7e0b427f
Arman Bilge <armanbilge@gmail.com>
2015-11-20 12:59:31 -0500

    Bug fixes for signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
+
+  validates :provider, :uid, :presence => true
+
   has_many :conferences
 
   class << self
     def from_omniauth(auth_hash)
-      user = find_or_create_by(uid: auth_hash[:uid], provider: auth_hash[:provider])
-      user.name = auth_hash[:info][:name]
-      user.save!
-      user
+      User.create_with(name: auth_hash[:info][:name]).find_or_create_by!(uid: auth_hash[:uid], provider: auth_hash[:provider])
     end
   end
 
