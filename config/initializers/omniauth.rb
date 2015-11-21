8f6999b311636d9d1c4f939b248999048ea9b4d4
Arman Bilge <armanbilge@gmail.com>
2015-11-21 03:23:46 -0500

    Set access type to online

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
     {
       :name => "google",
-      :prompt => "select_account"
+      :prompt => "select_account",
+      :access_type => "online"
     }
 end
