b2c0ac6c8ebc053d29ac44190ad6b2f41775e1bb
Arman Bilge <armanbilge@gmail.com>
2016-06-06 19:14:01 +1200

    Make affiliation optional

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -7,7 +7,6 @@ class Attendee < ActiveRecord::Base
   validates :last, presence: true
   validates :first, presence: true
   validates :email, presence: true, uniqueness: { scope: :conference, case_sensitive: false }, format: { with: /@/, on: :create }
-  validates :affiliation, presence: true
 
   def vital
     "#{last}, #{first} (#{affiliation})"
