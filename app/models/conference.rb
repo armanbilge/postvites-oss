3ae6ef29455a685c9b638488bd3a02919ba53f3e
Arman Bilge <armanbilge@gmail.com>
2016-12-23 20:14:49 -0800

    Bug fix

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -32,7 +32,7 @@ class Conference < ActiveRecord::Base
       self.attendees.delete_all
       self.keywords.delete_all
       CSV.foreach(path, headers: true, encoding: CharlockHolmes::EncodingDetector.detect(File.read(path))[:encoding]) do |row|
-        params = mapping.each_pair { |k, v| [k, row[v]] }.to_h
+        params = mapping.map { |k, v| [k, row[v]] }.to_h
         if params['keywords']
           params['keywords'] = params['keywords'].split(',').map { |k| self.keywords.find_or_create_by!(name: k.downcase) }
         else
@@ -54,14 +54,14 @@ class Conference < ActiveRecord::Base
     Conference.transaction do
       self.presenters.delete_all
       CSV.foreach(path, headers: true, encoding: CharlockHolmes::EncodingDetector.detect(File.read(path))[:encoding]) do |row|
-        attributes = mapping.each_pair { |k, v| [k, row[v]] }.to_h
+        params = mapping.map { |k, v| [k, row[v]] }.to_h
         secret = nil
         loop do
           secret = SecureRandom.hex
           break unless Presenter.exists?(secret: secret)
         end
-        attributes[:secret] = secret
-        self.presenters.create!(attributes)
+        params[:secret] = secret
+        self.presenters.create!(params)
       end
     end
   end
