202a292c4dd1b7d872927b5148e293fd6c083b2a
Arman Bilge <armanbilge@gmail.com>
2016-12-24 18:56:38 -0800

    Keyword uniqueness limited to conference

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,5 +2,5 @@ class Keyword < ActiveRecord::Base
   belongs_to :conference
   has_many :attendee_keywords
   has_many :attendees, through: :attendee_keywords
-  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates :name, presence: true, uniqueness: { case_sensitive: false, scope: :conference }
 end
