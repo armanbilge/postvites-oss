7fd069956275bfa3ef562e9060fbbdb057c7e469
Arman Bilge <armanbilge@gmail.com>
2016-12-25 17:46:54 -0800

    Use newer hash syntax

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Postvite
     config.active_job.queue_adapter = :delayed_job
 
     config.action_mailer.delivery_method = :postmark
-    config.action_mailer.postmark_settings = { :api_token => ENV['POSTMARK_API_TOKEN'] }
+    config.action_mailer.postmark_settings = { api_token: ENV['POSTMARK_API_TOKEN'] }
 
   end
 end
