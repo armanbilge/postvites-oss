b38e60791832bbfd1d9de4f36d08d1b1e7f049e5
Arman Bilge <armanbilge@gmail.com>
2017-02-22 22:43:34 -0800

    Fix contact page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,13 @@ class PagesController < ApplicationController
 
   def send_message
     params = send_message_params
-    ActionMailer::Base.mail(to: Rails.configuration.x.admin_email, subject: 'Message from postvites user', body: "#{params[:name]} <#{params[:email]}> wrote the following message:\n\n" + params[:message]).deliver_now
-    flash[:info] = 'Thanks, your message has been received.'
+    begin
+      ActionMailer::Base.mail(to: Rails.configuration.x.admin_email, from: "#{params[:name]} <#{params[:email]}>", subject: 'Message from postvites user', body: params[:message]).deliver_now
+      flash[:info] = 'Thanks, your message has been received.'
+    rescue Exception => e
+      flash[:danger] = e.message
+      redirect_to controller: 'pages', action: 'contact' and return
+    end
     redirect_to session[:referer].nil? ? root_path : session.delete(:referer)
   end
 
