ad1bf3d5e07be84300ac553fbebbef4f99356267
Arman Bilge <armanbilge@gmail.com>
2017-03-26 21:45:47 -0700

    Mark helpers private

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -6,6 +6,8 @@ class Invitation < ActiveRecord::Base
   validate :within_poster_limit, on: :create
   validate :within_invite_limit, on: :create
 
+  private
+
   def within_poster_limit
     errors[:base] << "#{attendee.first} #{attendee.last} cannot be invited to anymore posters." if attendee.presenters.count >= attendee.conference.poster_limit
   end
